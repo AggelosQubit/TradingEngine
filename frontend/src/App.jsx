6f45e23e373251594234174b693bcfec6a197f0b
AggelosQubit <aggelosqubit@gmail.com>
2024-01-15 22:09:17 +0100

    NN

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,13 +6,14 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" >
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" >
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
